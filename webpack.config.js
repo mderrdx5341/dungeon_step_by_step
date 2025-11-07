ee510f117e232bd425361d833a0bc3bcd9c5dc1f
Verkeev Roman <mderrdx5341@gmail.com>
2025-11-08 01:03:37 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+//const { ProvidePlugin } = require("webpack");
+const path = require('path');
+
+module.exports = {
+    externals: {
+        jquery : 'jQuery',
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+		}),
+        /*new ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+
+        })*/
+	],
+	entry: {
+		app: path.resolve(__dirname, './frontend/main.ts'),
+	},
+    devtool: 'source-map',
+    mode: "production", //production, development
+    stats: {
+        //errorDetails: true
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext][query]',
+                },
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext][query]',
+                },
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+			{
+				test: /\.(scss|css)$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+			},
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+
+        ],
+	},
+	output: {
+		path: path.resolve(__dirname, './www/'),
+		filename: '[name].js',
+	}
+}
